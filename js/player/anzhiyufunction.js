528f9d0b68318e6645bcf4dded9a0a0f9697d7f1
luaby <15537174991@163.com>
2025-05-13 09:15:10 +0000

    Update exe注册为windows服务.md UTC 2025-05-13 Tuesday 09:15:10 Updated By Github Actions

---
+++ js/player/anzhiyufunction.js
@@ -0,0 +1,7 @@
+// 音乐绑定事件
+function musicBindEvent() {
+    document.querySelector("#nav-music .aplayer-music").addEventListener("click", function () {
+        anzhiyu.musicTelescopic();
+    });
+}
+const metingJs = document.querySelector("#nav-music meting-js");
